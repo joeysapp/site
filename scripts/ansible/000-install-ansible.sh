eec0b835cdae0792435a691f7d5a3d136c82c193
joeysapp <sapp.joey@gmail.com>
2023-11-05 14:54:21 -0600

    [ansible] Adding simple base install script for digitalocean

---
+++ scripts/ansible/000-install-ansible.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# https://www.digitalocean.com/community/tutorials/how-to-install-and-configure-ansible-on-ubuntu-20-04
+sudo apt-add-respository ppa:ansible/ansible
+sudo apt update
+sudo apt install ansible
+
